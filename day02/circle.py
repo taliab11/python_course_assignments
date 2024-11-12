2464a601d6434a83f8f8431657325895cd77a765
Talia Baer <talia.baer@weizmann.ac.il>
2024-11-12 06:55:22 +0200

    Add files via upload

---
+++ day02/circle.py
@@ -0,0 +1,3 @@
+radius=5
+pi=3.141592653589793
+print("The area of the circle is", pi*radius**2, "and the circumference is", 2*pi*radius,".")
---
+++ day02/rectangle.py
@@ -0,0 +1,3 @@
+width=5
+length=8
+print("The area of the rectangle is", width*length, "and the perimeter is", 2*(width+length),".")
